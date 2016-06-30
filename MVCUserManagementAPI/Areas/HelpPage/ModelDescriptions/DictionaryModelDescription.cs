ba16e10322d7520b635d2804eaa1471ade9bc312
davvit <notMyRealEmail@gmail.com>
2016-06-30 18:11:01 -0500

    init vs template

---
+++ MVCUserManagementAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MVCUserManagementAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MVCUserManagementAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCUserManagementAPI.WebApiApplication" Language="C#" %>
